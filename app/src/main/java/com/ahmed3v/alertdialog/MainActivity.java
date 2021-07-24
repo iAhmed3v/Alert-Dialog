095acbbcd10fedd5f5135cca23d94626cf8b4a72
iAhmed3v <devahmdali@gmail.com>
2021-07-24 18:24:13 +0300

    Create DateActivity and DatePickerFragment

--- app/src/main/java/com/ahmed3v/alertdialog/MainActivity.java
+++ app/src/main/java/com/ahmed3v/alertdialog/MainActivity.java
@@ -2,17 +2,21 @@ package com.ahmed3v.alertdialog;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button button, anotherButton;
     private TextView textView;
 
     @Override
@@ -20,9 +24,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.alert_textView);
 
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.alert_button);
+        anotherButton = findViewById(R.id.button_next);
+
+        anotherButton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MainActivity.this, DateActivity.class);
+            startActivity(intent);
+        });
 
         button.setOnClickListener(v -> {
 
